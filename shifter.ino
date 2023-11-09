795721b1f5bcfeed79236dba4563cb59e9bf1d71
DillonTipton <129785439+DillonTipton@users.noreply.github.com>
2023-11-09 12:08:28 -0500

    Add files via upload

--- shifter.ino
+++ shifter.ino
@@ -1,27 +1,25 @@
 uint8_t buf[8] = { 0 }; //Keyboard report buffer
 
-#define PIN_IN A0 //Input pin
+#define PARK 4 //Input pin for park
+#define REVERSE 5 //Input pin for reverse
+#define NEUTRAL 6 //Input pin for neutral
+#define DRIVE 7 //Input pin for drive
+
+// May need to add more if we are implementing more states
 
 // Depending on how the shifter sends signals, or if we need to map more buttons, may need to declare more input pins
 
-// Voltage thresholds for each driving state (EXAMPLE VALUES, NOT CERTAIN AS OF YET)
-int driveThreshold = 128;
-int neutralThreshold = 96;
-int reverseThreshold = 64;
-int parkThreshold = 32;
+// Scaling factor
+int scale = 1;
 
 // Gear states are represented as integers (MAY NEED TO SCALE OR REARRANGE THEM IF WE NEED TO SEND THE SIGNAL FOR LONGER)
-int driveState = 4;
-int neutralState = 3;
-int reverseState = 2;
-int parkState = 1;
+int driveState = 4*scale;
+int neutralState = 3*scale;
+int reverseState = 2*scale;
+int parkState = 1*scale;
 
-int state = 1; // Target state read from input
-int currentState = 1; // What the vehicle is currently set to
-  
-// Keyboard signals to send
-int leftShift = (int) '<';
-int rightShift = (int) '>';
+int state = neutralState; // Target state read from input
+int currentState = neutralState; // What the vehicle is currently set to
 
 void setup() {
 
@@ -29,35 +27,38 @@ void setup() {
 
   //Set pinmode of Input pins
 
-  pinMode(PIN_IN, INPUT);
+  pinMode(PARK, INPUT);
+  pinMode(REVERSE, INPUT);
+  pinMode(NEUTRAL, INPUT);
+  pinMode(DRIVE, INPUT);
 
 }
 
 void loop() {
 
-  int input = analogRead(PIN_IN);
-
   // Set the target state (THESE HAVE TO BE IN DESCENDING ORDER)
-  if(input >= driveThreshold) { // If the shifter is set to drive
+  if(digitalRead(DRIVE) == HIGH) { // If the shifter is set to drive
     state = driveState;
-  } else if(input >= neutralThreshold) { // If the shifter is set to neutral
+  } else if(digitalRead(NEUTRAL) == HIGH) { // If the shifter is set to neutral
     state = neutralState;
-  } else if(input >= reverseThreshold) { // If the shifter is set to reverse
+  } else if(digitalRead(REVERSE) == HIGH) { // If the shifter is set to reverse
     state = reverseState;
-  } else if(input >= parkThreshold) { // If the shifter is set to park
+  } else if(digitalRead(PARK) == HIGH) { // If the shifter is set to park
     state = parkState;
   }
 
   // If the current state is a lower gear than the target gear, go up one gear
   if (state > currentState) {
-    Serial.write(buf, rightShift);
+    buf[2] = 27;
+    Serial.write(buf, 8);
     releaseKey();
     currentState++;
   }
 
   // If the current state is a higher gear than the target gear, go down a gear
   if (state < currentState) {
-    Serial.write(buf, leftShift);
+    buf[2] = 29;
+    Serial.write(buf, 8);
     releaseKey();
     currentState--;
   }
