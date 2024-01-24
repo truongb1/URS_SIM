f4595a21779d1b0ef0288f33cf3964ac54c8cdd0
DillonTipton <129785439+DillonTipton@users.noreply.github.com>
2024-01-24 10:32:51 -0500

    Add files via upload

--- Gear shifter/Main/shifter.ino
+++ Gear shifter/Main/shifter.ino
@@ -7,10 +7,11 @@ uint8_t buf[8] = { 0 }; //Keyboard report buffer
 
 #define RESET 8 //Input pin for reset button
 
-// Pins for the RGB LED
-#define RED A0
-#define GREEN A1
-#define BLUE A2
+// Pins for the Gear specific LEDs
+#define PARKLIGHT 9
+#define REVERSELIGHT 10
+#define NEUTRALLIGHT 11
+#define DRIVELIGHT 12
 
 // May need to add more if we are implementing more states
 
@@ -25,7 +26,8 @@ int neutralState = 3*scale;
 int reverseState = 2*scale;
 int parkState = 1*scale;
 
-int resetState = 17; // To ensure reset only gets sent once, uses the same state mechanic as the shift state
+int resetState = 17;
+int resetSim = 0;
 
 int state = parkState; // Target state read from input
 int currentState = state; // What the vehicle is currently set to
@@ -45,25 +47,31 @@ void setup() {
 
   //Set pinmode of output RGB pins
   
-  pinMode(RED, OUTPUT);
-  pinMode(GREEN, OUTPUT);
-  pinMode(BLUE, OUTPUT);
+  pinMode(PARKLIGHT, OUTPUT);
+  pinMode(REVERSELIGHT, OUTPUT);
+  pinMode(NEUTRALLIGHT, OUTPUT);
+  pinMode(DRIVELIGHT, OUTPUT);
 
 }
 
 void loop() {
 
   // Set the target state (THESE HAVE TO BE IN DESCENDING ORDER)
-  if(digitalRead(RESET) == HIGH) { // If the reset button is pressed
+  if(digitalRead(RESET) == HIGH && resetSim == 0) { // If the reset button is pressed
+    resetSim = 1;
     state = resetState;
   } else if(digitalRead(DRIVE) == HIGH) { // If the shifter is set to drive
-    state = driveState;
+     state = driveState;
   } else if(digitalRead(NEUTRAL) == HIGH) { // If the shifter is set to neutral
-    state = neutralState;
+     state = neutralState;
   } else if(digitalRead(REVERSE) == HIGH) { // If the shifter is set to reverse
     state = reverseState;
   } else if(digitalRead(PARK) == HIGH) { // If the shifter is set to park
     state = parkState;
+  }
+
+  if(digitalRead(RESET) == LOW) {
+    resetSim = 0;
   }
 
   // If the reset button is pressed, reset both the sim and the state variables
@@ -91,23 +99,27 @@ void loop() {
     currentState--;
   }
 
-  // Change LED color based on current state
-  if(currentState == parkState) { // Blue means park
-    analogWrite(RED, 0);
-    analogWrite(GREEN, 0);
-    analogWrite(BLUE, 255);
-  } else if(currentState == reverseState) { // Red means reverse
-    analogWrite(RED, 255);
-    analogWrite(GREEN, 0);
-    analogWrite(BLUE, 0);
-  } else if(currentState == neutralState) { // White means neutral
-    analogWrite(RED, 255);
-    analogWrite(GREEN, 255);
-    analogWrite(BLUE, 255);
-  } else if(currentState == driveState) { // Green means drive
-    analogWrite(RED, 0);
-    analogWrite(GREEN, 255);
-    analogWrite(BLUE, 0);
+  // Light up corresponding ring LED
+  if(currentState == parkState) {
+    digitalWrite(PARKLIGHT, HIGH);
+    digitalWrite(REVERSELIGHT, LOW);
+    digitalWrite(NEUTRALLIGHT, LOW);
+    digitalWrite(DRIVELIGHT, LOW);
+  } else if(currentState == reverseState) {
+    digitalWrite(PARKLIGHT, LOW);
+    digitalWrite(REVERSELIGHT, HIGH);
+    digitalWrite(NEUTRALLIGHT, LOW);
+    digitalWrite(DRIVELIGHT, LOW);
+  } else if(currentState == neutralState) {
+    digitalWrite(PARKLIGHT, LOW);
+    digitalWrite(REVERSELIGHT, LOW);
+    digitalWrite(NEUTRALLIGHT, HIGH);
+    digitalWrite(DRIVELIGHT, LOW);
+  } else if(currentState == driveState) {
+    digitalWrite(PARKLIGHT, LOW);
+    digitalWrite(REVERSELIGHT, LOW);
+    digitalWrite(NEUTRALLIGHT, LOW);
+    digitalWrite(DRIVELIGHT, HIGH);
   }
 
 }
